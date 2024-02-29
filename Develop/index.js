474ce2c3c2e695a8ef872538a0f253b2a826f473
m13corona <mcorona1308@gmail.com>
2024-02-28 18:22:44 -0600

    added packages and code for questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,12 +2,58 @@
 // done
 
 // TODO: Create an array of questions for user input
+const inquirer = require ('inquirer');
 const questions = [
-    
+    {
+
+    type: 'input',
+    name: 'name',
+    message: 'what is your name?'
+
+    },
+    {
+
+    type: 'confirm',
+    name: 'dev',
+    message: 'are you a developer?'
+
+    },
+    {
+
+    type: 'input',
+    name: 'age',
+    message: 'what year were you born in?'
+
+
+    },
+    {
+    type: 'list',
+    name: 'confirm',
+    message: 'The sky is blue',
+    default: true
+    }
 ];
+
+inquirer.prompt(questions)
+    .then(answers => {
+        console.log('Answers', answers);
+    })
+    .catch(error => {
+        console.log('Error', error);
+    })
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+const fs = require ('fs');
+
+fs.readFile('data.csv', 'utf8', (error, data) =>
+  error ? console.error(error) : console.log(data)
+);
+fs.writeFile('log.txt', process.argv[2], (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
 
 // TODO: Create a function to initialize app
 function init() {}
